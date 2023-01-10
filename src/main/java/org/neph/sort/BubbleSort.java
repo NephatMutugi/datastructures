47ade4d70439a23eec5eeceaf6ac378c98586633
KEN20960 <muchirinephat5@gmail.com>
2023-01-10 18:09:22 +0200

    Bubble Sort

--- src/main/java/org/neph/sort/BubbleSort.java
+++ src/main/java/org/neph/sort/BubbleSort.java
@@ -8,9 +8,8 @@ public class BubbleSort {
     private static void bubbleSort(int[] unsortedArr) {
         int temp;
         int n = unsortedArr.length;
-
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
+        for (int i = n-1; i >0; i--) {
+            for (int j = 0; j < i; j++) {
                 if (unsortedArr[j] > unsortedArr[j + 1]) {
                     temp = unsortedArr[j];
                     unsortedArr[j] = unsortedArr[j + 1];
