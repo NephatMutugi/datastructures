59ebb0807e8edf41d2bca571d056ced157e1283b
KEN20960 <muchirinephat5@gmail.com>
2023-01-11 17:38:16 +0200

    Selection Sort

---
+++ src/main/java/org/neph/sort/SelectionSort.java
@@ -0,0 +1,29 @@
+package org.neph.sort;
+
+/**
+ * @ Author NMuchiri
+ **/
+public class SelectionSort {
+    public static void main(String[] args) {
+
+        int[] arr = {20,35,-15, 7,55, 1, -22};
+
+    }
+
+    private static void sort(int [] arr){
+        int larger;
+        int arrLen = arr.length;
+
+        for (int i=0; i<arrLen; i++){
+            for (int j=0; j<i; j++){
+
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j){
+        if (i==j)
+            return;
+
+    }
+}
