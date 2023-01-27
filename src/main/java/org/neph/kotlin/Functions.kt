cb07bb9817936a7e45df15ea80329aa7fee3a5af
KEN20960 <muchirinephat5@gmail.com>
2023-01-27 15:45:05 +0200

    kotlin functions

--- src/main/java/org/neph/kotlin/Functions.kt
+++ src/main/java/org/neph/kotlin/Functions.kt
@@ -9,13 +9,23 @@ import com.google.gson.JsonObject
  **/
 
 fun main() {
+
+    myFunction()
+    println(addUp(5,3))
+    println(avg(3.2,5.3))
+
     val jsonString =
         "{\"corporateCreditInformationRecord\":{\"currencyType\":\"RWF\",\"country\":\"RW\",\"companyRegNo\":\"\",\"dateAccountUpdated\":\"20221129\",\"postalCode\":\"\",\"telephone3\":\"0788866618\",\"telephone2\":\"0788866618\",\"telephone1\":\"0788866618\",\"physicalAddressPlotNumber\":\"\",\"daysInArrears\":\"0\",\"facsimile1\":\"\",\"facsimile2\":\"\",\"institution\":\"SOEURS PALOTTINES REGION MASAKA\",\"emailAddress\":\"\",\"telephone6\":\"0788866618\",\"telephone5\":\"0788866618\",\"telephone4\":\"0788866618\",\"openingBalance\":\"5400000000\",\"companyRegistrationDate\":\"20170501\",\"interestRateatDisbursement\":\"\",\"sectorOfActivity\":\"Religious Institutions/Churches\",\"availableCredit\":\"5400000000\",\"companyCeaseDate\":\"\",\"accountRepaymentTerm\":\"BUL\",\"accountType\":\"I\",\"currentBalance\":\"5400000000\",\"taxNumber\":\"\",\"accountOwner\":\"P\",\"classification\":\"1\",\"physicalAddressDistrict\":\"NYARUGENGE\",\"scheduledPaymentAmount\":\"0\",\"actualPaymentAmount\":\"0\",\"dateAccountOpened\":\"20220210\",\"physicalAddressProvince\":\"UMUJYI WA KIGALI\",\"currentBalanceIndicator\":\"C\",\"postalAddressNumber\":\"\",\"approvalDate\":\"20221129\",\"physicalAddressSector\":\"Masaka\",\"dateAccountClosed\":\"20230101\",\"firstPaymentDate\":\"20230210\",\"industry\":\"Religious Institutions/Churches\",\"dateClosed\":\"\",\"accountStatus\":\"A\",\"termsDuration\":\"2\",\"lastPaymentDate\":\"20230101\",\"physicalAddressCell\":\"OL02000765\",\"physicalAddressLine2\":\"KICUKIRO\",\"tradingName\":\"SOEURS PALOTTINES REGION MASAKA\",\"physicalAddressLine1\":\"MASAKA KICUKIRO\",\"physicalAddressPostalCode\":\"\",\"nature\":\"23\",\"vatNo\":\"\",\"numberofLoanJoinParticipants\":\"\",\"accountNumber\":\"AA2233301PNW\",\"finalPaymentDate\":\"20230210\",\"installmentsInArrears\":\"0\",\"amountPastDue\":\"0\",\"category\":\"11\"},\"recordType\":\"CI\"}\n"
+    val gson = GsonBuilder().setPrettyPrinting().create()
 
+    val accountHolderString = gson.toJson(setAccountHolder(jsonString))
+    println("ACCOUNT HOLDER: \n$accountHolderString")
 
-    var gson = Gson()
+}
+
+fun setAccountHolder(jsonString: String): AccountHolder{
+    val gson = Gson()
     val jsonObject: JsonObject = gson.fromJson(jsonString, JsonObject::class.java)
-
     val accountOwner = jsonObject.get("corporateCreditInformationRecord").asJsonObject.get("accountOwner").asString
     val accountNumber = jsonObject.get("corporateCreditInformationRecord").asJsonObject.get("accountNumber").asString
     val physicalAddressLine1 = jsonObject.get("corporateCreditInformationRecord").asJsonObject.get("physicalAddressLine1").asString
@@ -24,11 +34,24 @@ fun main() {
     val industry = jsonObject.get("corporateCreditInformationRecord").asJsonObject.get("industry").asString
     val recordType = jsonObject.get("recordType").asString
 
-    val accountHolder = AccountHolder(accountOwner, accountNumber, physicalAddressLine1, physicalAddressLine2, accountStatus, industry, recordType)
-    gson = GsonBuilder().setPrettyPrinting().create()
-    val accountHolderString = gson.toJson(accountHolder)
-    println("ACCOUNT HOLDER: \n$accountHolderString")
+    return AccountHolder(accountOwner, accountNumber, physicalAddressLine1, physicalAddressLine2, accountStatus, industry, recordType)
 
+}
+// The syntax of a function - fun stands for function
+fun myFunction(){
+    // The body of a function
+    println("myFunction was called")
+}
+
+// This function has two parameters and returns a value of type Int
+fun addUp(a: Int, b: Int): Int{
+    // the return keyword indicates that this function will return the following value
+    // once this function is called and executed
+    return (a+b)
+}
+
+fun avg(a: Double, b: Double): Double {
+    return  (a + b)/2
 }
 
 data class AccountHolder(
