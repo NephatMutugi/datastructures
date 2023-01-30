c41a9fab345037a38e4dedf542add1bf723db0c9
KEN20960 <muchirinephat5@gmail.com>
2023-01-30 11:24:11 +0200

    data classes

--- src/main/java/org/neph/kotlin/oop/DataClasses.kt
+++ src/main/java/org/neph/kotlin/oop/DataClasses.kt
@@ -4,12 +4,11 @@ package org.neph.kotlin.oop
 @ Author NMuchiri
  **/
 
-data class User(val id: Long, val name: String)
-
 fun main(){
+
+    // USER
     val user1 = User(1, "Nephat Mutugi")
     println(user1)
-
     val updatedUser = user1.copy(id = 2)
     println(updatedUser)
 
@@ -19,4 +18,26 @@ fun main(){
     // deconstruction
     val (id, name) = updatedUser
     println("ID = $id, NAME = $name")
+
+
+    // MOBILE PHONE
+    val iphone = MobilePhone("iOS", "Apple", "iPhone 12")
+    val galaxyS20 = MobilePhone("Android","Samsung", "Galaxy S20")
+    val mateXS = MobilePhone("Android", "Huawei", "Mate X S")
+}
+data class User(val id: Long, val name: String)
+
+
+class MobilePhone(osName: String, brand: String, model: String){
+
+    private var battery = 30
+
+    init {
+        println("The phone $model from $brand uses $osName as its Operating System")
+    }
+
+    fun chargeBattery(chargedBy: Int){
+        println("Battery was at $battery and is at ${battery+chargedBy} now")
+        battery += chargedBy
+    }
 }
